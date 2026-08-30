32b83fb99a31182a2e3b9d27b0ea0a2e5a17720a
yannPGT <yannick.pagnot@icloud.com>
2026-08-30 18:47:37 +0200

    fix(widget): load questionnaire UI fixes

--- apps/widget/src/main.tsx
+++ apps/widget/src/main.tsx
@@ -2,5 +2,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App.js";
 import "./style.css";
+import "./questionnaire-fixes.css";
 
 createRoot(document.getElementById("root")!).render(<React.StrictMode><App /></React.StrictMode>);
